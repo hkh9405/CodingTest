aa2c5f216d965d92eea5cc53b9727cb1ccf8a2eb
hkh9405 <hkh9405@gmail.com>
2021-08-20 22:56:58 +0900

    코딩테스트 커밋 108일차

--- src/baekjoon/Num11650.java
+++ src/baekjoon/Num11650.java
@@ -1,59 +1,32 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.Scanner;
+import java.util.Arrays;
+ 
 public class Num11650 {
-	
-	public static int[] src;	
-	public static int[] tmp;
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
 		
-		int N=Integer.parseInt(br.readLine());
+		int N = in.nextInt();
 		
-		String[] s=new String[N];
-		Map<String, Integer> x=new HashMap<String, Integer>();
-		Map<String, Integer> y=new HashMap<String, Integer>();
+		int[][] arr = new int[N][2];
 		
-		for(int i=0;i<N;i++) {
-			s[i]=br.readLine();
-			
-			x.put(s[i], Integer.parseInt(s[i].split(" ")[0]));
-			y.put(s[i], Integer.parseInt(s[i].split(" ")[1]));
+		for(int i = 0; i < N; i++) {
+			arr[i][0] = in.nextInt();
+			arr[i][1] = in.nextInt();
 		}
 		
-		for(int i=0;i<N;i++) {
-			System.out.println(s[i]);
+		Arrays.sort(arr, (e1, e2) -> {
+			if(e1[0] == e2[0]) {
+				return e1[1] - e2[1];
+			} else {
+				return e1[0] - e2[0];
+			}
+		});
+		
+		for(int i = 0; i < N; i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
 		}
 	}
-
-	
-	public static void mergeSort(int start, int end) { 
-		if (start<end) { 
-			int mid = (start+end)/2;
-			mergeSort(start, mid);
-			mergeSort(mid+1, end);
-			
-			int p = start;
-			int q = mid + 1;
-			int idx = p;
-			
-			while (p<=mid || q<=end) {
-				if (q>end || (p<=mid && src[p]<src[q])) { 
-					tmp[idx++] = src[p++]; 
-				}else { 
-					tmp[idx++] = src[q++];
-				} 
-			}
-			
-			for (int i=start;i<=end;i++) {
-				src[i]=tmp[i]; 
-			}
-		}
-	}
-	
 }
