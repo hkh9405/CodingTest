13705029e86611e9429317814988031bed45951e
hkh9405 <hkh9405@gmail.com>
2021-08-28 20:44:53 +0900

    코딩테스트 커밋 115일차

--- src/baekjoon/Num18870.java
+++ src/baekjoon/Num18870.java
@@ -1,10 +1,7 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class Num18870 {
 	
@@ -13,23 +10,28 @@ public class Num18870 {
 		
 		int N=Integer.parseInt(br.readLine());
 		int[] num=new int[N];
-		List<Integer> list=new ArrayList<Integer>();
+		Map<Integer,Integer> map=new HashMap<Integer,Integer>();
 		
 		String[] str=br.readLine().split(" ");
 		
 		for(int i=0;i<N;i++) {
 			num[i]=Integer.parseInt(str[i]);
-			
-			if(!list.contains(num[i])) {
-				list.add(num[i]);
-			}
 		}
-		Collections.sort(list);
+		
+		int[] sortNum=num.clone();
+		Arrays.sort(sortNum);
 		
 		StringBuilder sb=new StringBuilder();
+		int index=0;
 		
-		for(int i=0;i<N;i++) {
-			sb.append(list.indexOf(num[i])+" ");
+		for(int i:sortNum) {
+			if(!map.containsKey(i)) {
+				map.put(i, index++);
+			}
+		}
+		
+		for(int i:num) {
+			sb.append(map.get(i)+" ");
 		}
 		
 		System.out.println(sb);
