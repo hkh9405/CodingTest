1d8c7cafddda26fd72fe3d21ab8711b65e4fe011
hkh9405 <hkh9405@gmail.com>
2021-08-30 23:07:23 +0900

    코딩테스트 커밋 117일차

--- src/baekjoon/Num15650.java
+++ src/baekjoon/Num15650.java
@@ -3,41 +3,38 @@ package baekjoon;
 import java.util.Scanner;
 
 public class Num15650 {
- 
-	private static int[] arr;
-	private static boolean[] visit;
+	 
+	public static int[] arr;
+	public static int N, M;
  
 	public static void main(String[] args) {
  
 		Scanner sc = new Scanner(System.in);
  
-		int N = sc.nextInt();
-		int M = sc.nextInt();
+		N = sc.nextInt();
+		M = sc.nextInt();
  
 		arr = new int[M];
-		visit=new boolean[N];
-		
-		dfs(N,M,0);
+        
+		dfs(1, 0);
+ 
 	}
-	
-	public static void dfs(int N,int M,int depth) {
-		if(M==depth) {
-			for(int i:arr) {
-				System.out.print(i+" ");
+ 
+	public static void dfs(int at, int depth) {
+ 
+		if (depth == M) {
+			for (int val : arr) {
+				System.out.print(val + " ");
 			}
-			
 			System.out.println();
 			return;
 		}
-		
-		for(int i=0;i<N;i++) {
-			if(!visit[i]) {
-				visit[i]=true;
-				arr[depth]=i+1;
-				dfs(N,M,depth+1);
-			}
+        
+		for (int i = at; i <= N; i++) {
+ 
+			arr[depth] = i;
+			dfs(i + 1, depth + 1);
+ 
 		}
 	}
- 
-	
 }
